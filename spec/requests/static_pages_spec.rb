841a2a55dfbd996f318e34902a7889f3ad6f8907
Benjamin Swieskowski <benswieskowski@gmail.com>
2013-09-18 16:54:13 -0700

    Changed title

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,7 +6,7 @@ describe "Static pages" do
 
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
+      expect(page).to have_content('JK')
     end
 
     it "should have the title 'Home'" do
